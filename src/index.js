4fcb3af583806a33099a4cd9970ab4a530613e64
Thigo <thiago.l17@yahoo.com>
2022-09-27 22:59:44 -0300

    To-do Project

--- src/index.js
+++ src/index.js
@@ -54,14 +54,28 @@ app.post('/users', (request, response) => {
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request
-  console.log(user)
+  // console.log(user)
   
-  console.log(users)
+  // console.log(users)
   return response.json(user.todos).send();
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
+  const { title, deadline } = request.body;
+  const { user } = request;
   
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo)
+  console.log(todo)
+  return response.status(201).json(todo)
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
